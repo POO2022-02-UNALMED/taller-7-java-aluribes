2c6a4640cdf0b29617837df46ba616cfbc1d3597
Alejandra Uribe <aluribes@unal.edu.co>
2022-10-11 04:50:45 -0500

    Create Periodico.java
    Periódico

---
+++ src/main/java/comunicacion/Periodico.java
@@ -0,0 +1,54 @@
+package comunicacion;
+
+public class Periodico extends Escrito {
+	private String fecha;
+	private String primicia;
+	private String interpretacion;
+
+	// Constructor
+	public Periodico(String origen, String titulo, String autor, int paginas, String fecha, String primicia,
+					 String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.fecha = fecha;
+		this.primicia = primicia;
+		this.interpretacion = interpretacion;
+	}
+
+	// Get y Set
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	
+	public String getPrimicia() {
+		return primicia;
+	}
+	public void setPrimicia(String primicia) {
+		this.primicia = primicia;
+	}
+	
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+
+	// Métodos
+	@Override
+	public int palabrasTotales(int palabrasPagina){
+		return palabrasPagina*getPaginas()*10;
+	}
+	
+	@Override
+	public String interpretacion(){
+		return getInterpretacion();
+	}
+	
+	@Override
+	public String toString() {
+		return super.getOrigen()+ "\n" + super.getTitulo()+ "\n" + super.getAutor() + "\n"+ super.getPaginas()+ "\n"+this.fecha + "\n"+this.primicia;
+	}
+}
